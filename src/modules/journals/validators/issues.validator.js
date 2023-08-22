d758e54ee5695b89ea4eb98b6bece9f72c16a54c
ccgtnn <ccgtnn@gmail.com>
2023-08-22 17:27:52 +0500

    исправление num -> number в issues.validator

--- src/modules/journals/validators/issues.validator.js
+++ src/modules/journals/validators/issues.validator.js
@@ -4,7 +4,7 @@ export const issuesValidator = yup.object({
   id: yup.number().integer().required(),
   name: yup.string().required(),
   year: yup.number().integer().default(0),
-  num: yup.number().integer().default(0),
+  number: yup.number().integer().default(0),
   ord: yup.number().integer().default(0),
   dateCreate: yup.number().integer().default(0),
 })
